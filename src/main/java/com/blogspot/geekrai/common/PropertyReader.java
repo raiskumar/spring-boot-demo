4bb0c53c85205c809320221ffa352a66901fca07
Siddheshwar Kumar <rai.skumar@gmail.com>
2017-11-23 19:13:53 +0530

    added support to read from dynamic environment

--- src/main/java/com/blogspot/geekrai/common/PropertyReader.java
+++ src/main/java/com/blogspot/geekrai/common/PropertyReader.java
@@ -37,6 +37,34 @@ public class PropertyReader {
     public String getFromMesssagesProperties(String key){
         return messageSource.getMessage(key, null, Locale.ENGLISH);
     }
+    
+    /**
+     * Get System/Application properties for the given key
+     * Note: It gives preference to the environment over the application property
+     */
+    public String getProperty(String key) {
+        Objects.requireNonNull(key, "Key is mandatory");
+
+        String val = getPropertyFromDynamicEnvironment(key);
+
+        return !Objects.isNull(val)
+                ? val
+                : getFromApplicationProperties(key);
+    }
+    
+    /**
+     * Provides any value set in System/Shell environment
+     * Or, parameters passed to JVM
+     * Reads from SystemEnvironment
+     */
+    private String getPropertyFromDynamicEnvironment(String key) {
+        Objects.requireNonNull(key, "Key is mandatory");
+
+        if (!Objects.isNull(System.getProperty(key))) {
+            return System.getProperty(key);  // jvm parameter using -D
+        }
+        return System.getenv(key);  // From System environment
+    }
 
 
 }
